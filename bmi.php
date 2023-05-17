929b74e83443754538f1b2f27dc8ca1a6c035f11
Lillie McAlister <lilliemcalister1993@live.com>
2023-05-17 15:36:34 -0700

    completed bmi php calc page

--- bmi.php
+++ bmi.php
@@ -6,24 +6,27 @@
         $height = $_POST["height"];
     }
     if(isset( $_POST["weight"])) {
-        $height = $_POST["weight"];
+        $weight = $_POST["weight"];
 }
-    if(isset( $_POST["submit"])) {
+    if(isset( $_POST["calculate"])) {
         $bmi = ($weight / ($height * $height)) * 703;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en"
-<head>
-    <title>BMI Calculator</title>
-</head>
-
-<h1>Please enter your information to calculate your BMI</h1>
-
+<html>
+<h1>Please enter your information to calculate your BMI:</h1>
 <?php
-
-
-
-?>
-
+ $form = <<<FORM
+<form name="bmi" method="post" actions="/bmi.php">
+    <p><label name="weight">Weight in Pounds</label>
+        <lable><input name="weight" type="number" value="$weight"></lable>
+    </p>
+    <p><label name="height">Height in Inches</label>
+        <lable><input name="height" type="number" value="$height"></lable>
+    </p>
+    <p><input name="calculate" type="submit" value="Calculate BMI"></p>
+</form>
+FORM;
+ echo $form;
+ echo "<p>Your BMI is: $bmi</p>"
+ ?>
+</html>
